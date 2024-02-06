1853b636007a8be48c1a349c9f2a782ccea20f6b
legion112 <ageev.maks2014@gmail.com>
2024-02-06 15:55:19 +0400

    FindTheIndexOfTheFirstOccurrenceInString

---
+++ FindTheIndexOfTheFirstOccurrenceInString/FindTheIndexOfTheFirstOccurrenceInString.go
@@ -0,0 +1,20 @@
+package FindTheIndexOfTheFirstOccurrenceInString
+
+func strStr(haystack string, needle string) int {
+	needleCharacters := []rune(needle)
+	lastIndex := len(needleCharacters) - 1
+	haystackCharacters := []rune(haystack)
+	for i := 0; i < len(haystackCharacters) && (i+len(needleCharacters) <= len(haystackCharacters)); i++ {
+		index := 0
+		for j, needleChar := range needleCharacters {
+			index = i + j
+			if haystackCharacters[index] != needleChar {
+				break
+			}
+			if j == lastIndex {
+				return i
+			}
+		}
+	}
+	return -1
+}
